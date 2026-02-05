d9771672b6bf8a96d371f449dfa508faf3f77017
osek15 <kacperossowski12@gmail.com>
2026-02-05 18:54:49 +0100

    Update NWD.py

--- NWD.py
+++ NWD.py
@@ -1,16 +1,9 @@
-def nwd_iteracyjnie(a, b):
+def nwd(a, b):
     while b != 0:
         a, b = b, a % b
     return a
 
-def nwd_rekurencyjnie(a, b):
-    if b == 0:
-        return a
-    return nwd_rekurencyjnie(b, a % b)
-
 if __name__ == "__main__":
-    test_a, test_b = 112, 42
+    x, y = 123456, 789
 
-    print(f"Test dla liczb {test_a} i {test_b}:")
-    print(f"Wynik (iteracyjnie): {nwd_iteracyjnie(test_a, test_b)}")
-    print(f"Wynik (rekurencyjnie): {nwd_rekurencyjnie(test_a, test_b)}")
+    print(f"Wynik: {nwd(x, y)}")
