ee9a608cb498d412850b70051a041dbde877381d
osek15 <kacperossowski12@gmail.com>
2026-02-05 19:03:47 +0100

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,1 +1,12 @@
+def bubble_sort(arr):     
+  n = len(arr)     
+  for i in range(n):         
+    for j in range(n - i - 1):             
+      if arr[j] > arr[j + 1]:                 
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]     
+        
+  return arr
 
+if __name__ == "__main__":
+    test = [64, 34, 25, 12, 22, 11, 90]
+    print(f"Wynik: {bubble_sort(test)}")
