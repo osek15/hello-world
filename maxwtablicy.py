029b502b6fb9bae3e837fd21a4ac4d462323ecf1
osek15 <kacperossowski12@gmail.com>
2026-02-05 19:22:34 +0100

    Create maxwtablicy.py

---
+++ maxwtablicy.py
@@ -0,0 +1,12 @@
+def znajdz_max(tab):
+    if not tab:
+        return None
+    max_val = tab[0]
+    for i in range(1, len(tab)):
+        if tab[i] > max_val:
+            max_val = tab[i]
+    return max_val
+
+if __name__ == "__main__":
+    tab = [4, 6, 2, 1, 3]
+    print(f"Max = {znajdz_max(tab)}")
